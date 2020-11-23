ebd0de92f85b1e26fd69fb4115eb2988aa78999c
sambaths <sambath.s20@bimtech.ac.in>
2020-11-23 23:07:29 +0530

    1 - 23/11/2020 - added download_from_kaggle

---
+++ kaggle_helpers.py
@@ -0,0 +1,32 @@
+
+import subprocess
+import os
+
+
+def download_from_kaggle(comp_name, print_bash_output=False):
+  if 'COLAB_GPU' in os.environ:
+    files = os.listdir()
+    if 'kaggle.json' not in files:
+        print('Please upload your kaggle.json file to connect with Kaggle API !!')
+        from google.colab import files
+        files.upload()
+    try:
+        files = os.listdir('input/')
+    except:
+        files=[]
+    if 'train.csv' not in files:
+        bashCmds = ["pip uninstall -y kaggle", "pip install --upgrade pip", "pip install -q kaggle==1.5.6", "mkdir -p ~/.kaggle","cp kaggle.json ~/.kaggle/", "chmod 600 ~/.kaggle/kaggle.json",
+        "mkdir input", "kaggle competitions download -c comp_name", "sudo unzip -q -n '*.zip' -d 'input/'", "sudo rm *.zip"]
+        for cmd in bashCmds:
+          if 'comp_name' not in cmd:
+            try:
+              process = subprocess.run(cmd,shell=True, check=True, stdout=subprocess.PIPE)
+            except:
+              pass
+          else:
+            cmd = f"kaggle competitions download {comp_name}"
+            process = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE)
+          if print_bash_output:
+              for out in process.stdout.decode('utf-8').split('\n'):
+                  print(out)
+
